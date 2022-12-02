b8d4a8492f6246fe5384f0ef4d436f869ba80102
Jan Reppien <jan.reppien@uni-bielefeld.de>
2022-12-02 09:41:08 +0100

    added solution for day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import dayOne.DayOne
+import dayTwo.DayTwo
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
     val dayOne = DayOne()
     dayOne.solveAndPrint()
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val dayTwo = DayTwo()
+    dayTwo.solveAndPrint()
 }
---
+++ src/main/kotlin/dayTwo/DayTwo.kt
@@ -0,0 +1,89 @@
+package dayTwo
+
+import AocSolution
+import java.io.File
+
+class DayTwo : AocSolution(2) {
+
+    private val input = readInput()
+
+    private fun readInput(): MutableList<List<Int>> {
+        val inputs = mutableListOf<List<Int>>()
+        val file = File("src/main/resources/inputs/dayTwo/input.txt")
+
+        file.forEachLine {
+            inputs.add(it.split(' ').map { signToNumber(it[0]) })
+        }
+        return inputs
+    }
+
+    private fun signToNumber(char: Char): Int {
+        return when (char) {
+            'A' -> 1
+            'B' -> 2
+            'C' -> 3
+            'X' -> 1
+            'Y' -> 2
+            'Z' -> 3
+            else -> 0
+        }
+    }
+
+    private fun score(game: List<Int>): List<Int> {
+        val input = game.toMutableList()
+        if (input[0] == 1 && input[1] == 3) {
+            return listOf(7,3)
+        }
+        if (input[0] == 3 && input[1] == 1) {
+            return listOf(3,7)
+        }
+        if (input[0] < input[1]) {
+            return listOf(input[0],input[1] + 6)
+        }
+        if (input[0] == input[1]) {
+            return listOf(input[0] + 3, input[1] + 3)
+        }
+        if (input[0] > input[1]) {
+            return listOf(input[0] + 6, input[1])
+        }
+        return listOf()
+    }
+
+    override fun solvePartOne(): String {
+        var sum = 0
+        input.forEach {
+            sum += score(it)[1]
+        }
+        return sum.toString()
+    }
+
+    private fun strategy(game: List<Int>): List<Int> {
+        if(game[1] == 1) {
+            if(game[0] == 1) {
+                return listOf(7,3)
+            }
+            return listOf(game[0]+6,game[0]-1)
+        }
+
+        if(game[1] == 2) {
+            return listOf(game[0]+3,game[0]+3)
+        }
+
+        if(game[1] == 3) {
+            if(game[0] == 3) {
+                return listOf(3,7)
+            }
+            return listOf(game[0],game[0]+7)
+        }
+
+        return listOf()
+    }
+
+    override fun solvePartTwo(): String {
+        var sum = 0
+        input.forEach {
+            sum += strategy(it)[1]
+        }
+        return sum.toString()
+    }
+}
